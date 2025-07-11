4b583ef5bbd318fdf7d10d8744fbd7e2ec41a5e5
DevObaloluwa <securedbyoba@gmail.com>
2025-07-11 14:13:22 +0100

    fix: point Jest roots to src only

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 // jest.config.js
 module.exports = {
-  roots: ['<rootDir>/src'],              // ← only src here
+  roots: ['<rootDir>/src'],              // ← drop the test/ root
   testRegex: '.*\\.(spec|test)\\.ts$',
   transform: { '^.+\\.(t|j)s$': 'ts-jest' },
   moduleFileExtensions: ['ts', 'js', 'json'],
