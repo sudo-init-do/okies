01dadc2a23b18f621e824f1df624ed0b4361349e
DevObaloluwa <securedbyoba@gmail.com>
2025-07-11 14:03:23 +0100

    test: configure Jest and add smoke test

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,23 @@
 // jest.config.js
 module.exports = {
+  // look in src/ and test/ folders
   roots: ['<rootDir>/src', '<rootDir>/test'],
+
+  // match either .spec.ts or .test.ts
   testRegex: '.*\\.(spec|test)\\.ts$',
-  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+
+  // use ts-jest for TS
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+
   moduleFileExtensions: ['ts', 'js', 'json'],
+
+  // collect coverage from all TS/JS files
   collectCoverageFrom: ['**/*.(t|j)s'],
+
   testEnvironment: 'node',
-  passWithNoTests: true
+
+  // don’t fail if there are literally no tests
+  passWithNoTests: true,
 };
