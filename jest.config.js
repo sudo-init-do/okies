3200c606c26b0f47e5c5692c77a4dc40830ef0c8
DevObaloluwa <securedbyoba@gmail.com>
2025-07-11 14:10:21 +0100

    fix: remove unused test/ root from Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,10 @@
 // jest.config.js
 module.exports = {
-  // look in src/ and test/ folders
-  roots: ['<rootDir>/src', '<rootDir>/test'],
-
-  // match either .spec.ts or .test.ts
+  roots: ['<rootDir>/src'],              // ← only src here
   testRegex: '.*\\.(spec|test)\\.ts$',
-
-  // use ts-jest for TS
-  transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
-  },
-
+  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
   moduleFileExtensions: ['ts', 'js', 'json'],
-
-  // collect coverage from all TS/JS files
   collectCoverageFrom: ['**/*.(t|j)s'],
-
   testEnvironment: 'node',
-
-  // don’t fail if there are literally no tests
   passWithNoTests: true,
 };
